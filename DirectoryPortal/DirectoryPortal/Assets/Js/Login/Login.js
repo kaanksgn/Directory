6686f014b903362b04de21944fa61394e312dc37
kaank <kaankesgin4@gmail.com>
2022-08-06 13:02:01 +0300

    Sign Up session added.

--- DirectoryPortal/DirectoryPortal/Assets/Js/Login/Login.js
+++ DirectoryPortal/DirectoryPortal/Assets/Js/Login/Login.js
@@ -24,3 +24,42 @@
 
     });
 }
+
+function Register() {
+
+    var name = jQuery('#inputNameRegister').val();
+    var email = jQuery('#inputEmailRegister').val();
+    var password = jQuery('#inputPhoneRegister').val();
+    var phone = jQuery('#inputPasswordRegister').val();
+    var passwordAgain = jQuery('#inputPasswordAgainRegister').val();
+
+
+    if (password != passwordAgain) {
+        alert("Passwords are not the same");
+
+    } else {
+
+        var params = "?name=" + name + "?email=" + email + "&password=" + password  "&phone=" + phone;
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:50706/api/Authanticate/Register" + params,
+            cache: false,
+            success: function (data) {
+
+                if (data !== null && data !== undefined && data !== "undefined" && data !== "") {
+                    alert("Register Successfull.")
+                } else {
+
+                    alert("Error accured while Process.");
+                }
+            },
+            error: function (xhr, txtStatus, errorThrown) {
+                alert("Error Code : " + xhr.status + " " + txtStatus + "\n" + errorThrown);
+
+            }
+
+
+        });
+    }
+
+}
